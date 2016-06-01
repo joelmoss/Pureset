5fb96e2032705a65197836e242099518623b6102
Joel Moss <joel@developwithstyle.com>
2016-06-02 00:21:28 +0100

    Styled page title and fixed header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,20 @@ module ApplicationHelper
   def icon(*names, id: nil)
     content_tag :i, nil, class: names.map { |name| name.to_s.tr('_', '-') } << 'icon', id: id
   end
+
+  def page_title_for_layout
+    if @page_title
+      content_tag :h1 do
+        content_tag :div, @page_title, class: 'container'
+      end
+    end
+  end
+
+  def page_title_for_head
+    "#{@page_title} | " if @page_title
+  end
+
+  def page_title(text)
+    @page_title = text
+  end
 end
