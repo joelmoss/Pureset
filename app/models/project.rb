942c4cbcb46d8f0a3bea9c85f37a8e2c280798b9
Joel Moss <joel@developwithstyle.com>
2016-06-02 23:24:24 +0100

    Validating Project#name

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,9 @@ class Project < ApplicationRecord
   belongs_to :account, polymorphic: true
   acts_as_url :name, url_attribute: :slug, blacklist: %w{ users organizations issues projects }
 
+  validates :name, presence: true,
+                   uniqueness: { case_sensitive: false }
+
   def to_param
     slug
   end
