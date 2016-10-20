b6021d3c6faf1867dfffe0d7af6869cd679d1346
Joel Moss <joel@developwithstyle.com>
2016-10-20 18:47:42 +0100

    Enable inline source maps in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,6 +47,9 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  # Enable inline source maps
+  config.sass.inline_source_maps = true
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
